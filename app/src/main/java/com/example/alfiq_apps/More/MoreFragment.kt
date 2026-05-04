bec14d8f4d86cfc03a41a7b3b38d81478f9197c1
alfiqD <alfiq24si@mahasiswa.pcr.ac.id>
2026-05-04 09:30:38 +0700

    Commit Tugas P8 ListView

--- app/src/main/java/com/example/alfiq_apps/More/MoreFragment.kt
+++ app/src/main/java/com/example/alfiq_apps/More/MoreFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.SimpleAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.alfiq_apps.R
 import com.example.alfiq_apps.databinding.FragmentMessageBinding
@@ -16,6 +19,39 @@ class MoreFragment : Fragment() {
     private var _binding: FragmentMoreBinding? = null
     private val binding get() = _binding!!
 
+    private val dataList = listOf(
+        "Kotlin",
+        "Java",
+        "Python",
+        "C++",
+        "JavaScript",
+        "Dart",
+        "Swift",
+        "Go",
+        "Ruby",
+        "R",
+        "PHP",
+        "C#",
+        "TypeScript",
+        "Shell",
+        "SQL",
+        "Perl",
+        "Rust",
+        "Scala",
+        "Haskell",
+        "Lua",
+        "Erlang",
+        "Prolog",
+        "Assembly",
+        "Objective-C",
+        "VBA"
+    )
+
+    private val dataListWithDesc = listOf(
+        mapOf("title" to "Kotlin", "desc" to "Bahasa untuk Android modern"),
+        mapOf("title" to "Java", "desc" to "Bahasa OOP yang populer"),
+        mapOf("title" to "Python", "desc" to "Bahasa yang mudah dipahami")
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,11 +61,33 @@ class MoreFragment : Fragment() {
 
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
         (requireActivity() as AppCompatActivity).supportActionBar?.apply {
             title = "More"
         }
+
+            //Ubah adapter menjadi seperti berikut
+            val adapter = SimpleAdapter(
+                requireContext(),
+                dataListWithDesc,
+                android.R.layout.simple_list_item_2,
+                arrayOf("title", "desc"),
+                intArrayOf(android.R.id.text1, android.R.id.text2)
+            )
+
+            // Hubungkan listViewItems dengan adapter (masih sama dengan sebelumnya)
+            binding.listViewItems.adapter = adapter
+
+            // Tambahkan aksi saat item di-list diklik
+            binding.listViewItems.setOnItemClickListener { _, _, position, _ ->
+                val selectedItem = dataListWithDesc[position]
+                val title = selectedItem["title"]
+                val desc = selectedItem["desc"]
+                Toast.makeText(requireContext(), "Kamu memilih: $title ($desc)", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
-}
